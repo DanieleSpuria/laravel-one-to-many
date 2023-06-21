c03936c2d5d04857722dbff91f4c98ebac7e3130
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-21 16:03:06 +0200

    add foreign key update_projects

--- database/migrations/2023_06_21_132027_update_projects_table.php
+++ database/migrations/2023_06_21_132027_update_projects_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        // Schema::table('projects', function (Blueprint $table) {
-        //     $table->unsignedBigInteger('type_id');
-        //     $table->foreign('type_id')
-        //           ->references('id')
-        //           ->on('type')
-        //           ->onDelete('set null');
-        // });
+        Schema::table('projects', function (Blueprint $table) {
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+            $table->foreign('type_id')
+                  ->references('id')
+                  ->on('types')
+                  ->onDelete('set null');
+        });
     }
 
     /**
@@ -29,9 +29,9 @@ return new class extends Migration
      */
     public function down()
     {
-        // Schema::table('projects', function (Blueprint $table) {
-        //     $table->dropForeign(['type_id']);
-        //     $table->dropColumn('type_id');
-        // });
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign(['type_id']);
+            $table->dropColumn('type_id');
+        });
     }
 };
